41e245d9cc7f64fa8ce8140cf2d245344f520900
Boris Veselov <borisveselov@yahoo.com>
2024-02-16 12:17:26 -0500

    add menu to stress page

--- mind-walk/src/pages/StressReliefPage.js
+++ mind-walk/src/pages/StressReliefPage.js
@@ -3,7 +3,26 @@ import React from 'react';
 const StressReliefPage = () => {
   return (
     <div>
-      <h2>Stress Relief</h2>
+      <h2>Stress Management</h2>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur fermentum sem, id tempus felis. Mauris maximus erat non lacus lobortis facilisis eu sed tortor. 
+        Donec nibh erat, varius vel arcu at, posuere posuere leo. Nulla dignissim elit eget ligula maximus placerat. Donec sollicitudin purus eget purus cursus varius. Integer tempus dolor nec tincidunt pharetra. 
+        Integer tristique accumsan ullamcorper. Sed tristique nunc lorem, ac suscipit ante congue sit amet. Quisque imperdiet justo nec enim tempus, sit amet pretium dolor consectetur. 
+        </p>
+
+      <li className="nav-item">
+        <a className="nav-link" href="#">headspace</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">journal</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">excercises</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">tools</a>
+      </li>
+
 
     </div>
   );
